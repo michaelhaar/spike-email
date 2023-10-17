378521d7a44d6235fae663cc9a6aff9fc2fe6cce
Michael Haar <michael.haar92@gmail.com>
2023-10-17 12:55:30 +0200

    add email template switch

--- index.ts
+++ index.ts
@@ -2,11 +2,31 @@
 import { createElement } from "react";
 import { renderToString } from "react-dom/server";
 import { WelcomeEmail } from "./src/email-templates/welcome";
+import { OrderConfirmationEmail } from "./src/email-templates/order-confirmation";
 
-export async function getEmailContent() {
-  return renderToString(createElement(WelcomeEmail));
+type EmailTemplateId = "welcome" | "order-confirmation";
+
+export function getEmailTemplateComponent(emailTemplateId: EmailTemplateId) {
+  switch (emailTemplateId) {
+    case "welcome":
+      return WelcomeEmail;
+    case "order-confirmation":
+      return OrderConfirmationEmail;
+    default:
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-case-declarations
+      const _exhaustiveCheck: never = emailTemplateId;
+      throw new Error(`Invalid emailTemplateId: ${emailTemplateId}`);
+  }
 }
 
-getEmailContent().then((emailContent) => {
-  console.log(emailContent);
+export async function getEmailHtml(
+  emailTemplateId: EmailTemplateId
+): Promise<string> {
+  const emailComponent = getEmailTemplateComponent(emailTemplateId);
+
+  return renderToString(createElement(emailComponent));
+}
+
+getEmailHtml("order-confirmation").then((emailHtml) => {
+  console.log(emailHtml);
 });
---
+++ src/email-templates/order-confirmation.tsx
@@ -0,0 +1,8 @@
+export function OrderConfirmationEmail() {
+  return (
+    <div>
+      <h1>Order Confirmation</h1>
+      <p>Thank you for your order.</p>
+    </div>
+  );
+}
