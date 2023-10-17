13839a2e3c5a8751beb179226778bc642df97e50
Michael Haar <michael.haar92@gmail.com>
2023-10-17 12:46:00 +0200

    add first email template

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello world");
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createElement } from "react";
+import { renderToString } from "react-dom/server";
+import { WelcomeEmail } from "./src/email-templates/welcome";
+
+export async function getEmailContent() {
+  return renderToString(createElement(WelcomeEmail));
+}
+
+getEmailContent().then((emailContent) => {
+  console.log(emailContent);
+});
---
+++ src/email-templates/welcome.tsx
@@ -0,0 +1,8 @@
+export const WelcomeEmail = () => {
+  return (
+    <div>
+      <h1>Welcome Michael</h1>
+      <p>Thanks for signing up for our service.</p>
+    </div>
+  );
+};
